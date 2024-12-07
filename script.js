3d4802dab940e1a1e9093c73af034c11f854a31a
NikaMartynchuk <veronika.martynchuk@gmail.com>
2024-12-07 14:10:26 +0200

    .

--- script.js
+++ script.js
@@ -1,74 +1,48 @@
-
-//Створити масив з трьох чисел. Змінити значення другого елемента масиву на 10.
-let numbers = [1, 2 ,3]
-numbers[1] = 10
-console.log(numbers)
-
-//Створити масив із трьох рядків. Додати до масиву ще одну рядків.
-let row = [
-    ["Ketrin", "Viktor"],
-    ["Andrew", "Nika"],
-    ["Sasha", "Ellie"]
-]
-
-row[3] = ["Vlad", "Timophey"]
-console.log(row)
+const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+let string = '';
+for (let i = 0; i < friends.length; i++) {
+  string += friends[i] + (i < friends.length - 1 ? ',' : '');
+}
+console.log(string); 
 
 
-//Створити скрипт який поверне суму всіх чисел в масиві.
-function sumArray(numbers) {
-    let sum = 0;
-    for (let num of numbers) {
-        sum += num; 
-    }
-    return sum;
+
+const friend = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+const str = friend.join(',');
+console.log(str); 
+
+
+
+
+const cards = ['Карточка-1', 'Карточка-2', 'Карточка-3', 'Карточка-4', 'Карточка-5'];
+
+const cardToRemove = 'Карточка-3';
+
+const indexToRemove = cards.indexOf(cardToRemove);
+
+if (indexToRemove !== -1) {
+  cards.splice(indexToRemove, 1);
 }
 
-const array = [1, 2, 3, 4, 5];
-console.log(sumArray(array));
+console.log(cards); 
 
 
-//Створити масив з 5-ти чисел. Вивести на екран всі елементи масиву за допомогою циклу for.
 
-const number = [10, 20, 30, 40, 50];
 
-for (let i = 0; i < number.length; i++) {
-    console.log(number[i]);
+const cardToInsert = 'Карточка-6';
+const indexToInsert = 2; 
+
+cards.splice(indexToInsert, 0, cardToInsert);
+
+console.log(cards); 
+
+const cardToUpdate = 'Карточка-4';
+const newCard = ' Карточка-4';
+
+const indexToUpdate = cards.indexOf(cardToUpdate);
+
+if (indexToUpdate !== -1) {
+  cards.splice(indexToUpdate, 1, newCard);
 }
 
-
-
-// Створити масив із 5-ти рядків. Вивести на екран кожен рядокз масиву, який містить більше 5-ти символів.
-const strings = ["apple", "banana", "cat", "elephant", "dog"];
-
-for (let str of strings) {
-    if (str.length > 5) {
-        console.log(str); 
-    }
-}
-
-
-//Створити масив з 10-ти чисел. Знайти та вивести на екран максимальне значення з масиву.
-
-const dataArray = [14, 67, 23, 89, 35, 42, 90, 12, 76, 53];
-
-let highestValue = dataArray[0]; 
-
-for (let item of dataArray) {
-    if (item > highestValue) {
-        highestValue = item; 
-    }
-}
-
-console.log("Максимальне значення в масиві:", highestValue);
-
-
-
-//Створити масив з 10-ти чисел. Знайти всі парні числа в масиві та вивести їх на екран.
-const numArra = [12, 7, 25, 40, 33, 18, 21, 50, 8, 19];
-
-for (let currentNum of numArra) {
-    if (currentNum % 2 === 0) { 
-        console.log(currentNum); 
-    }
-}
+console.log(cards); 
