4e976bce080886d276f83d9addca25ce60178e93
NikaMartynchuk <veronika.martynchuk@gmail.com>
2025-02-09 10:59:24 +0200

    .

--- script.js
+++ script.js
@@ -1,121 +1,71 @@
+// Завдання 1
+const categoriesList = document.querySelector('#categories');
+const categoryItems = categoriesList.querySelectorAll('.item');
+console.log(`У списку ${categoryItems.length} категорії.`);
 
+categoryItems.forEach(item => {
+    const title = item.querySelector('h2').textContent;
+    const elementsCount = item.querySelectorAll('li').length;
+    console.log(`Категорія: ${title}\nКількість елементів: ${elementsCount}`);
+});
 
-const users = [
-  {
-    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
-    name: 'Moore Hensley',
-    email: 'moorehensley@indexia.com',
-    eyeColor: 'blue',
-    friends: ['Sharron Pace'],
-    isActive: false,
-    balance: 2811,
-    skills: ['ipsum', 'lorem'],
-    gender: 'male',
-    age: 37,
-  },
-  {
-    id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
-    name: 'Sharlene Bush',
-    email: 'sharlenebush@tubesys.com',
-    eyeColor: 'blue',
-    friends: ['Briana Decker', 'Sharron Pace'],
-    isActive: true,
-    balance: 3821,
-    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
-    gender: 'female',
-    age: 34,
-  },
-  {
-    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
-    name: 'Ross Vazquez',
-    email: 'rossvazquez@xinware.com',
-    eyeColor: 'green',
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-    isActive: false,
-    balance: 3793,
-    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
-    gender: 'male',
-    age: 24,
-  },
-  {
-    id: '249b6175-5c30-44c6-b154-f120923736f5',
-    name: 'Elma Head',
-    email: 'elmahead@omatom.com',
-    eyeColor: 'green',
-    friends: ['Goldie Gentry', 'Aisha Tran'],
-    isActive: true,
-    balance: 2278,
-    skills: ['adipisicing', 'irure', 'velit'],
-    gender: 'female',
-    age: 21,
-  },
-  {
-    id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
-    name: 'Carey Barr',
-    email: 'careybarr@nurali.com',
-    eyeColor: 'blue',
-    friends: ['Jordan Sampson', 'Eddie Strong'],
-    isActive: true,
-    balance: 3951,
-    skills: ['ex', 'culpa', 'nostrud'],
-    gender: 'male',
-    age: 27,
-  },
-  {
-    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
-    name: 'Blackburn Dotson',
-    email: 'blackburndotson@furnigeer.com',
-    eyeColor: 'brown',
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
-    isActive: false,
-    balance: 1498,
-    skills: ['non', 'amet', 'ipsum'],
-    gender: 'male',
-    age: 38,
-  },
-  {
-    id: 'e1bf46ab-7168-491e-925e-f01e21394812',
-    name: 'Sheree Anthony',
-    email: 'shereeanthony@kog.com',
-    eyeColor: 'brown',
-    friends: ['Goldie Gentry', 'Briana Decker'],
-    isActive: true,
-    balance: 2764,
-    skills: ['lorem', 'veniam', 'culpa'],
-    gender: 'female',
-    age: 39,
-  },
+// Завдання 2
+const ingredients = [
+    'Картопля',
+    'Гриби',
+    'Часник',
+    'Помідори',
+    'Зелень',
+    'Приправи',
 ];
 
+const ingredientsList = document.querySelector('#ingredients');
+const ingredientElements = ingredients.map(ingredient => {
+    const li = document.createElement('li');
+    li.textContent = ingredient;
+    return li;
+});
 
-//Отримати загальну суму балансу (поле balance) всіх користувачів.
+ingredientsList.append(...ingredientElements);
 
-const totalBalance = users.reduce((sum, user) => sum + user.balance, 0);
-console.log(totalBalance); 
+// Завдання 3
+const images = [
+    {
+        url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'White and Black Long Fur Cat',
+    },
+    {
+        url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    },
+    {
+        url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'Group of Horses Running',
+    },
+];
 
+const gallery = document.querySelector('#gallery');
+gallery.style.display = 'flex';
+gallery.style.gap = '10px';
+gallery.style.listStyle = 'none';
 
-//Масив імен всіх користувачів у яких є друг із зазначеним ім'ям.
+gallery.insertAdjacentHTML(
+    'beforeend',
+    images.map(({ url, alt }) => `<li><img src="${url}" alt="${alt}" width="300"></li>`).join('')
+);
 
-const getUsersWithFriend = (friendName) =>
-  users
-    .filter(user => user.friends.includes(friendName))
-    .map(user => user.name);
+// Завдання 4
+let counterValue = 0;
+const valueSpan = document.querySelector('#value');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
 
-console.log(getUsersWithFriend('Sharron Pace'));
+decrementBtn.addEventListener('click', () => {
+    counterValue -= 1;
+    valueSpan.textContent = counterValue;
+});
 
-//Масив імен (поле name) людей, відсортованих в залежності від кількості їх друзів (поле friends)
-
-const sortedByFriendsCount = users
-  .sort((a, b) => a.friends.length - b.friends.length)
-  .map(user => user.name);
-
-console.log(sortedByFriendsCount);
-
-
-//Завдання 4 тримати масив всіх умінь всіх користувачів (поле skills), при цьому не має бути повторюваних умінь і вони повинні бути відсортовані в алфавітному порядку.
-
-const uniqueSkills = Array.from(
-  new Set(users.flatMap(user => user.skills))
-).sort();
-
-console.log(uniqueSkills); 
+incrementBtn.addEventListener('click', () => {
+    counterValue += 1;
+    valueSpan.textContent = counterValue;
+});
