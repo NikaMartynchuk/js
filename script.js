44e0d7c0b6c244a2e1878ee213a9f8362c2f3c5b
NikaMartynchuk <veronika.martynchuk@gmail.com>
2025-04-06 15:20:12 +0300

    ..

--- script.js
+++ script.js
@@ -1,71 +1,23 @@
-// Завдання 1
-const categoriesList = document.querySelector('#categories');
-const categoryItems = categoriesList.querySelectorAll('.item');
-console.log(`У списку ${categoryItems.length} категорії.`);
+// Завдання 1: Слайдер масштабу
+const slider = document.querySelector('.slider__input');
+const image = document.querySelector('.slider__image');
 
-categoryItems.forEach(item => {
-    const title = item.querySelector('h2').textContent;
-    const elementsCount = item.querySelectorAll('li').length;
-    console.log(`Категорія: ${title}\nКількість елементів: ${elementsCount}`);
+const updateImageScale = _.debounce((value) => {
+  const scale = value / 50; 
+  image.style.transform = `scale(${scale})`;
+}, 100);
+
+slider.addEventListener('input', (e) => {
+  updateImageScale(e.target.value);
 });
 
-// Завдання 2
-const ingredients = [
-    'Картопля',
-    'Гриби',
-    'Часник',
-    'Помідори',
-    'Зелень',
-    'Приправи',
-];
+const box = document.getElementById('box');
 
-const ingredientsList = document.querySelector('#ingredients');
-const ingredientElements = ingredients.map(ingredient => {
-    const li = document.createElement('li');
-    li.textContent = ingredient;
-    return li;
-});
+const moveBox = _.debounce((x, y) => {
+  box.style.left = x + 'px';
+  box.style.top = y + 'px';
+}, 10);
 
-ingredientsList.append(...ingredientElements);
-
-// Завдання 3
-const images = [
-    {
-        url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        alt: 'White and Black Long Fur Cat',
-    },
-    {
-        url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-    },
-    {
-        url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        alt: 'Group of Horses Running',
-    },
-];
-
-const gallery = document.querySelector('#gallery');
-gallery.style.display = 'flex';
-gallery.style.gap = '10px';
-gallery.style.listStyle = 'none';
-
-gallery.insertAdjacentHTML(
-    'beforeend',
-    images.map(({ url, alt }) => `<li><img src="${url}" alt="${alt}" width="300"></li>`).join('')
-);
-
-// Завдання 4
-let counterValue = 0;
-const valueSpan = document.querySelector('#value');
-const decrementBtn = document.querySelector('[data-action="decrement"]');
-const incrementBtn = document.querySelector('[data-action="increment"]');
-
-decrementBtn.addEventListener('click', () => {
-    counterValue -= 1;
-    valueSpan.textContent = counterValue;
-});
-
-incrementBtn.addEventListener('click', () => {
-    counterValue += 1;
-    valueSpan.textContent = counterValue;
+document.addEventListener('mousemove', (e) => {
+  moveBox(e.clientX, e.clientY);
 });
